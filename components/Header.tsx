cacd33e9f2e0d7c220d206350cc358612c8ad737
felixbuhler <felix.buhler@me.com>
2022-09-16 23:47:46 +0200

    First Idea

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='sticky bg-white z-10 p-2 w-full flex flex-nowrap border-blue-700 border'>
+        <h1>Kiffel</h1>
+        <div className='logo'>LOGO</div>
+        
+    </header>
+  )
+}
+
+export default Header
