4a0846b5be6ce82d20aec3bf0f619a179f8dad28
felixbuhler <felix.buhler@me.com>
2022-09-22 17:23:32 +0200

    New Build with Vanilla JS

--- kiffel.js
+++ kiffel.js
@@ -5,50 +5,82 @@ var valueVierer = 0;
 var valueFuenfer = 0;
 var valueSechser = 0;
 
+var dreierpasch = 0;
+var viererpasch = 0;
+var fullHouse = 0;
+var kleineStrasse = 0;
+var grosseStrasse = 0;
+var kniffel = 0;
+var valueChance = 0;
+
 var gesamtObenOhneBonus = 0;
 var gesamtObenMitBonus = 0;
+
+var gesamtUnten = 0;
+
+var gesamt = 0;
 
 function getValEinser() {
     var val = parseInt(document.getElementById("valueEinser").value);
     window['valueEinser'] = val;
     getGesamtObenOhneBonus();
-
-
+    updateGesamt()
 }
 
 function getValZweier() {
     var val = parseInt(document.getElementById("valueZweier").value);
     window['valueZweier'] = val;
     getGesamtObenOhneBonus();
-
+    updateGesamt()
 }
 
 function getValDreier() {
     var val = parseInt(document.getElementById("valueDreier").value);
     window['valueDreier'] = val;
     getGesamtObenOhneBonus();
-
+    updateGesamt()
 }
 
 function getValVierer() {
     var val = parseInt(document.getElementById("valueVierer").value);
     window['valueVierer'] = val;
     getGesamtObenOhneBonus();
-
+    updateGesamt()
 }
 
 function getValFuenfer() {
     var val = parseInt(document.getElementById("valueFuenfer").value);
     window['valueFuenfer'] = val;
     getGesamtObenOhneBonus();
-
+    updateGesamt()
 }
 
 function getValSechser() {
     var val = parseInt(document.getElementById("valueSechser").value);
     window['valueSechser'] = val;
     getGesamtObenOhneBonus();
+    updateGesamt()
+}
 
+function getValChance() {
+    var val = parseInt(document.getElementById("valueChance").value);
+    window['valueChance'] = val;
+    getGesamtUnten();
+    updateGesamt()
+}
+
+function getValDreierpasch() {
+    var val = parseInt(document.getElementById("valueDreierpasch").value);
+    window['dreierpasch'] = val;
+    getGesamtUnten();
+    updateGesamt()
+}
+
+function getValViererpasch() {
+    var val = parseInt(document.getElementById("valueViererpasch").value);
+    window['viererpasch'] = val;
+    getGesamtUnten();
+    updateGesamt()
 }
 
 
@@ -70,7 +102,68 @@ function getGesamtObenOhneBonus() {
         document.getElementById("gesamt-oben-mit-bonus").innerHTML = gesamtObenMitBonus;
         document.getElementById("gesamt-oben-mit-bonus-end").innerHTML = gesamtObenMitBonus;
     }
+}
 
-    console.log("Einser =", valueEinser);
-    console.log("Gesamt ohne Bonus =", gesamtObenOhneBonus);
+function getValFullHouse() {
+    var checkElement = document.getElementById('full-house');
+    if (checkElement.checked) {
+        window['fullHouse'] = 25;
+    } else {
+        window['fullHouse'] = 0;
+    }
+    getGesamtUnten();
+    updateGesamt()
+}
+
+function getValKleineStrasse() {
+    var checkElement = document.getElementById('kleine-strasse');
+    if (checkElement.checked) {
+        window['kleineStrasse'] = 30;
+    } else {
+        window['kleineStrasse'] = 0;
+    }
+    getGesamtUnten();
+    updateGesamt()
+}
+function getValGrosseStrasse() {
+    var checkElement = document.getElementById('grosse-strasse');
+    if (checkElement.checked) {
+        window['grosseStrasse'] = 40;
+    } else {
+        window['grosseStrasse'] = 0;
+    }
+    getGesamtUnten();
+    updateGesamt()
+}
+function getValKniffel() {
+    var checkElement = document.getElementById('kniffel');
+    if (checkElement.checked) {
+        window['kniffel'] = 50;
+    } else {
+        window['kniffel'] = 0;
+    }
+    getGesamtUnten();
+    updateGesamt()
+}
+
+
+
+function getGesamtUnten() {
+    var calcGesamtUnten = fullHouse + kleineStrasse + grosseStrasse + kniffel + valueChance + dreierpasch + viererpasch;
+    window['gesamtUnten'] = calcGesamtUnten;
+    document.getElementById("gesamt-unten").innerHTML = calcGesamtUnten;
+
+    updateGesamt()
+}
+
+function getGesamt() {
+
+    window['gesamt'] = window['gesamtUnten'] + window['gesamtObenMitBonus'];
+    console.log("untenGesamt", window['gesamt']);
+
+}
+
+function updateGesamt() {
+    getGesamt();
+    document.getElementById("gesamt").innerHTML = window['gesamt'];
 }
---
+++ tooltips.js
@@ -0,0 +1,52 @@
+/* Tooltips */
+
+tippy('#info-einser', {
+    content: 'Nur Einser zählen',
+});
+
+tippy('#info-zweier', {
+    content: 'Nur Zweier zählen',
+});
+
+tippy('#info-dreier', {
+    content: 'Nur Dreier zählen',
+});
+
+tippy('#info-vierer', {
+    content: 'Nur Vierer zählen',
+});
+
+tippy('#info-fuenfer', {
+    content: 'Nur Fünfer zählen',
+});
+
+tippy('#info-sechser', {
+    content: 'Nur Sechser zählen',
+});
+
+tippy('#info-dreierpasch', {
+    content: 'Alle Augen zählen',
+});
+
+tippy('#info-viererpasch', {
+    content: 'Alle Augen zählen',
+});
+
+tippy('#info-fullhouse', {
+    content: '25 Punkte',
+});
+
+tippy('#info-kleinestrasse', {
+    content: '30 Punkte',
+});
+tippy('#info-grossestrasse', {
+    content: '40 Punkte',
+});
+
+tippy('#info-kniffel', {
+    content: '50 Punkte',
+});
+
+tippy('#info-chance', {
+    content: 'Alle Augen zählen',
+});
