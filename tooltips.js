f155fbfd087d1936ff3d22b10f9dbb7440dde837
felixbuhler <felix.buhler@me.com>
2022-09-22 17:40:06 +0200

    New Build with Vanilla JS

--- tooltips.js
+++ tooltips.js
@@ -2,51 +2,64 @@
 
 tippy('#info-einser', {
     content: 'Nur Einser zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-zweier', {
     content: 'Nur Zweier zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-dreier', {
     content: 'Nur Dreier zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-vierer', {
     content: 'Nur Vierer zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-fuenfer', {
     content: 'Nur Fünfer zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-sechser', {
     content: 'Nur Sechser zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-dreierpasch', {
     content: 'Alle Augen zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-viererpasch', {
     content: 'Alle Augen zählen',
+    theme: 'kiffel'
 });
 
 tippy('#info-fullhouse', {
     content: '25 Punkte',
+    theme: 'kiffel'
 });
 
 tippy('#info-kleinestrasse', {
     content: '30 Punkte',
+    theme: 'kiffel'
 });
 tippy('#info-grossestrasse', {
     content: '40 Punkte',
+    theme: 'kiffel'
 });
 
 tippy('#info-kniffel', {
     content: '50 Punkte',
+    theme: 'kiffel'
 });
 
 tippy('#info-chance', {
     content: 'Alle Augen zählen',
+    theme: 'kiffel'
 });
